6fa45543e03bfa78ac0b9ad105ee1edd518b4745
bonghee <hae8064@nate.com>
2022-02-22 00:24:40 +0900

    Test

--- FlutterLogin-main/FlutterLogin-main/lib/fireStroe.dart
+++ FlutterLogin-main/FlutterLogin-main/lib/fireStroe.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//컬렉션 필드값 읽기
 void readData() {
   final userCollectionReference =
       FirebaseFirestore.instance.collection("member").doc("user");
