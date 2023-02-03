74744dd05f23526acee5f0d829f2b5f028d4891e
hssum9409 <99657701+hssum9409@users.noreply.github.com>
2023-02-03 10:25:00 +0900

    Update DoorlockControlModule.py
    AES-128 Decryption 기능 추가

--- DoorlockControlModule.py
+++ DoorlockControlModule.py
@@ -1,137 +1,106 @@
-#-*-conding:utf-8 -*-
 import time
-import os, ntpath
-import json
-import subprocess
 import atexit
-
 from datetime import datetime
 
-#import RPi.GPIO as GPIO
-from flask import Flask
-from flask import request
+import RPi.GPIO as GPIO
 
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from firebase_admin import db
+from firebase_admin import credentials, firestore, db
 
-### Config Info Setup ###
-CONTROL_PIN = 17        # Doorlock OPEN Relay 신호 핀 넘버
-ROOM_TYPE = 'Standard'  # Doorlock Roomtype (type 'str')
-DELAY_TIME = 0.1        # Doorlock OPEN 신호 주기 (SEC, type 'float')
-AUTH_FILENAME = 'flutterfirebaselogin-fe24a-firebase-adminsdk-9grre-6c770a417f.json'
-                        # Firebase Auth Filename (type 'str')
-LOG_FILENAME = './LogFile/logFile.txt'
-                        # Log File route/name.txt (type 'str')
-RECEIVING_IP = ''
-                        # IP Address of RaspberryPI (type 'str')
+import base64
+from Crypto.Cipher import AES
 
-### Global Init ###
+def exit_handler():
+    print("Exiting program")
 
-logFile = open(LOG_FILENAME, 'a')
-logFile.write(f'Device Init... at {str(datetime.now())}\n')
-logFile.close()
+    logFile.close()
 
+atexit.register(exit_handler)
+
+def divide_string(string):
+    return string.split("/")
+
+def decrypt_aes_128(key_b64, iv_b64, encrypted_text):
+    # Decode the key and IV from base64
+    key = base64.b64decode(key_b64)
+    iv = base64.b64decode(iv_b64)
+
+    # Initialize the cipher with the decoded key and IV
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    # Decode the encrypted text from base64 and decrypt it
+    encrypted_bytes = base64.b64decode(encrypted_text)
+    decrypted_text = cipher.decrypt(encrypted_bytes)
+
+    # Remove the padding from the decrypted text
+    padding = decrypted_text[-1]
+    decrypted_text = decrypted_text[:-padding]
+    
+    return decrypted_text.decode()
+
+def divide_string(string):
+    # Divide String by '/' and return divided String Array
+    return string.split("/")
+
+### Init Setting ###
+GPIO_PIN_NUMS = [5, 6, 22, 27]          # Doorlock Relay 신호 Pin 넘버 (Std >> 5 // Dlx >> 6 // Exc >> 22 // Sup >> 27)
+DELAY_TIME = 0.1                        # Doorlock OPEN 신호 주기 (SEC, type 'float')
+AUTH_FILENAME = '/home/pi/Projects/flutterfirebaselogin-fe24a-firebase-adminsdk-9grre-6c770a417f.json'
+                                        # Firebase Auth Filename (type 'str')
+LOG_FILENAME = '/home/pi/Projects/LogFile/logFile.txt'
+                                        # Log File route/name.txt (type 'str')
+GPIO.setmode(GPIO.BCM)                  # GPIO Pin 넘버 기준 (GPIO.BCM / GPIO.BOARD)
+
+### Firebase Setting ###
 cred = credentials.Certificate(AUTH_FILENAME)
 firebase_admin.initialize_app(cred)
 client = firestore.client()
 
 ORDER_COLLECTION_REF = client.collection(u'order').document(u'raspberrypi')
 
-#GPIO.setmode(GPIO.BCM)
-Flask_APP = Flask(__name__)
+### Logfile Init ###
+logFile = open(LOG_FILENAME, 'a')
+logFile.write(f'Device Init... at {str(datetime.now())}\n')
 
+while 1:
+    print("### loop ###")
+    GPIO.setup(GPIO_PIN_NUMS, GPIO.IN)
 
-def firebaseInfoLoad(userEmail, roomtype):
-    REQUEST_USER_REF=client.collection(u'member').document(userEmail).get().to_dict()
+    ORDER_SET = ORDER_COLLECTION_REF.get().to_dict()
+    order_content = ORDER_SET[u'order']
 
-    if REQUEST_USER_REF == None:
-        return -1
+    if order_content == True:
+        order_info = divide_string(decrypt_aes_128(ORDER_SET[u'key'], ORDER_SET[u'ivkey'], ORDER_SET[u'value']))
+            # order_info[0] >> Email // order_info[1] >> RoomType // order_info[2] >> Reservation info
 
-    if REQUEST_USER_REF[u'reservation'] == True:
-        if REQUEST_USER_REF[u'rooms'] == ROOM_TYPE:
-            return 0
+        member_info = client.collection(u'member').document(order_info[0]).get().to_dict()
 
-        else:
-            return -3
+        if member_info[u'reservation'] == True and member_info[u'rooms'] == order_info[1] :
 
-    else:
-        return -2
+            if member_info[u'rooms'] == 'Standard' :
+                channel = 5
+            elif member_info[u'rooms'] == 'Deluxe' :
+                channel = 6
+            elif member_info[u'rooms'] == 'Executive' :
+                channel = 22
+            elif member_info[u'rooms'] == 'Superior' :
+                channel = 27
+            
+            print('Door lock controlled...')
+            print(member_info[u'email'])
+            print(member_info[u'rooms'])
 
-@Flask_APP.route('/open',methods=['POST'])
-def openRequest():
-    ### Open Request by User
-    requestData = request.get_json()
-    userEmail = requestData['email']    # Request User Email
-    roomtype = requestData['roomtype']  # Request Roomtype
+            logFile.write(str(datetime.now()))
+            logFile.write("\tDoorlock Open by ")
+            logFile.write(order_info[0])
+            logFile.write("...\n")
+				
+            GPIO.setup(channel,GPIO.OUT)
+            GPIO.output(channel,True)
+            time.sleep(0.1)
+            GPIO.setup(channel,GPIO.IN)
 
-    if roomtype == ROOM_TYPE:
-        requestReturn = firebaseInfoLoad(userEmail, roomtype)
-
-        if requestReturn == 0:
-            ### 정상 처리
-            print(f"'[{userEmail}] open at {str(datetime.now())}")
-            logFile = open(LOG_FILENAME, 'a')
-            logFile.write(f'[Control Success] Doorlock Controlled at {str(datetime.now())} by [{userEmail}]\n')
-            logFile.close()
-
-            ### Control GPIO to Open Doorlock
-#            GPIO.setup(CONTROL_PIN, GPIO.OUT)
-#            GPIO.output(CONTROL_PIN, True)
-#            time.sleep(DELAY_TIME)
-#            GPIO.setup(CONTROL_PIN, GPIO.IN)
-
-            ### Set Order Collection Empty
-            ORDER_COLLECTION_REF.set({u'order' : ""})
-
-        elif requestReturn == -1:
-            ### Member Collection 내에 Email 부재
-            print("Error >> User not found.")
-            logFile = open(LOG_FILENAME, 'a')
-            logFile.write(f'[Control Fail >> User not found.] at {str(datetime.now())} by [{userEmail}]\n')
-            logFile.close()
-
-            ### Set Order Collection Empty
-            ORDER_COLLECTION_REF.set({u'order' : ""})
-
-        elif requestReturn == -2:
-            ### user의 예약상태가 false 임
-            print("Error >> User Reservation Status is FALSE.")
-            logFile = open(LOG_FILENAME, 'a')
-            logFile.write(f'[Control Fail >> User Reservation Status is FALSE.] at {str(datetime.now())} by [{userEmail}]\n')
-            logFile.close()
-
-            ### Set Order Collection Empty
-            ORDER_COLLECTION_REF.set({u'order' : ""})
-
-        elif requestReturn == -3:
-            ### user의 예약 호실 정보와 요청 호실 정보가 일치하지 않음
-            print("Error >> User Reservation Info does not match.")
-            logFile = open(LOG_FILENAME, 'a')
-            logFile.write(f'[Control Fail >> User Reservation Info does not match.] at {str(datetime.now())} by [{userEmail}]\n')
-            logFile.close()
-
-            ### Set Order Collection Empty
-            ORDER_COLLECTION_REF.set({u'order' : ""})
-
-    else:
-        ### Requested Roomtype과 Device Roomtype이 일치하지 않음.
-        print("Error >> Request Roomtype and Device Roomtype does not match")
-        logFile = open(LOG_FILENAME, 'a')
-        logFile.write(f'[Control Fail >> Request Roomtype and Device Roomtype does not match] at {str(datetime.now())} by [{userEmail}]\n')
-        logFile.close()
-
-        ### Set Order Collection Empty
-        ORDER_COLLECTION_REF.set({u'order' : ""})
-
-def handle_exit():
-    logFile = open(LOG_FILENAME, 'a')
-    logFile.write(f'[Exit] at {str(datetime.now())}\n\n')
-    logFile.close()
-
-if __name__ == "__main__":
-    atexit.register(handle_exit)
-#    GPIO.setup(17,GPIO.IN)
-
-    Flask_APP.run(host = RECEIVING_IP)
+            channel = 0
+            
+            ORDER_COLLECTION_REF.set({u'ivkey' : "", u'key' : "", u'order' : False, u'value' : ""})
+    time.sleep(1)
